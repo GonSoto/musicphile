adcc7c1f5ec8028afe5ebb42d7ecc981d76ea945
GonSoto <gonsotomayor@gmail.com>
2026-06-03 21:38:28 +0200

    Setup Flask project structure, styled centered login and register pages. Style absolutely horrible for now.

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
